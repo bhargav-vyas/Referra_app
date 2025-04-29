88157fe8c38a3893aec36b9bf8770eda8619a2a4
Bhargav <vyasb1503@gmail.com>
2025-04-29 22:19:54 +0530

    this is the  first  step to creat  mean   the class is created and  in the layout the ui is created

---
+++ app/src/main/java/com/example/referal/LoginRequest.java
@@ -0,0 +1,10 @@
+package com.example.referal;
+public class LoginRequest {
+    private String email;
+    private String password;
+
+    public LoginRequest(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+}
---
+++ app/src/main/java/com/example/referal/LoginResponse.java
@@ -0,0 +1,11 @@
+package com.example.referal;
+
+public class LoginResponse {
+        private String message; // or token, or userId, based on your backend response
+
+        public String getMessage() {
+            return message;
+        }
+    }
+
+
